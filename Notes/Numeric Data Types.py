343b8cbba36ba3f3980012dfa84daef022d9e744
jasonc621 <149524312+jasonc621@users.noreply.github.com>
2024-09-26 12:28:22 -0400

    Create Numeric Data Types.py

---
+++ Notes/Numeric Data Types.py
@@ -0,0 +1,34 @@
+#Numeric Data Types
+
+#ints - integer number, just like in math: 1, 2, 3, -1, -2, -3, 0, 500, 1000, 51024, -9999
+#floats - decimal numers: 1.9, 3.7, 1.7, 2.0
+
+#2 vs. 2.0: Python sees 2 as the int,2 but it sees 2.0 and as the float, 2.0 and 2.
+
+#Strings - sequences of characters: 'A13', 'Hello, World', '4'. 
+#Strings are denoted with single quotes. Technically you can get away with double quotes, 
+#and occasionally, double quotes are needed but professionally python needed 
+
+#Each character in a string has a positon (aka an index). The First
+#characters incex is 0. The next character's is 1, etc. For example,
+#the 0th character in 'Hello, World' is H. The 6th character in 
+#'Hello, World' is a space.
+
+#Operations:
+
+# + means add with ints:
+
+print(2 + 3)
+
+# + means add floats
+
+print(2.0 + 1.5)
+
+# + means add when combining an int and a float.  
+#When Python adds an int to a float the result is a float.
+print(2 + 3.0)
+
+#means __ when combining strings.
+print('Computer' + 'Science')
+print('10' + '5')
+
